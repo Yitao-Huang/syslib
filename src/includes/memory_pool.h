fa0efb8aec426e4df6b0b6b50bfef2d2a64eda55
Yitao Huang <yitao@umich.edu>
2024-09-02 14:38:48 -0700

    thread safe memory pool

--- src/includes/memory_pool.h
+++ src/includes/memory_pool.h
@@ -1,9 +1,14 @@
+#include <atomic>
+#include <cstdlib>
 #include "logger.h"
 
+/*
+    not thread safe pool allocator
+*/
 class memory_pool {
 public:
-    struct Block {
-        Block* next;
+    struct block {
+        block* next;
     };
 
     memory_pool(size_t block_size, size_t num_blocks)
@@ -16,11 +21,11 @@ public:
             exit(1);
         }
 
-        free_list = (Block*)pool_start;
-        Block* curr = free_list;
+        free_list = (block*)pool_start;
+        block* curr = free_list;
         for (size_t idx = 1; idx < num_blocks; idx++) 
         {
-            curr->next = (Block*)((char*)pool_start + idx * block_size);
+            curr->next = (block*)((char*)pool_start + idx * block_size);
             curr = curr->next;
         }
         curr->next = nullptr;
@@ -32,16 +37,15 @@ public:
         {
             return nullptr;
         }
-
-        void* res = (Block*)free_list;
+        void* res = (block*)free_list;
         free_list = free_list->next;
         return res;
     }
 
-    void deallocate(void* block)
+    void deallocate(void* ptr)
     {
-        Block* curr = free_list;
-        free_list = (Block*)block;
+        block* curr = free_list;
+        free_list = (block*)ptr;
         free_list->next = curr;
     }
 
@@ -51,8 +55,73 @@ public:
     }
 
 private:
-    size_t block_size_;
-    size_t num_blocks_;
     void* pool_start;
-    Block* free_list;
+    block* free_list;
+};
+
+
+/*
+    thread safe pool allocator
+*/
+class safe_memory_pool {
+public:
+    struct block {
+        block* next;
+    };
+
+    safe_memory_pool(size_t block_size, size_t num_blocks)
+    {
+        pool_start = malloc(num_blocks * block_size);
+        if (pool_start == nullptr)
+        {
+            LOG(LogLevel::ERROR, "malloc failed during memory pool init")
+            exit(1);
+        }
+
+        free_list = (block*)pool_start;
+        block* curr = free_list;
+        for (size_t idx = 1; idx < num_blocks; ++idx) 
+        {
+            curr->next = (block*)((char*)pool_start + idx * block_size);
+            curr = curr->next;
+        }
+        curr->next = nullptr;
+    }
+
+    void* allocate()
+    {
+        block* old_head = free_list.load(std::memory_order_acquire);
+
+        while (old_head) 
+        {
+            block* new_head = old_head->next;
+            if (free_list.compare_exchange_weak(old_head, new_head, std::memory_order_acquire, std::memory_order_relaxed)) 
+            {
+                return old_head;
+            }
+        }
+
+        return nullptr; // Pool is empty
+    }
+
+    void deallocate(void* ptr)
+    {
+        block* new_head = (block*)ptr;
+        block* old_head = free_list.load(std::memory_order_acquire);
+
+        do 
+        {
+            new_head->next = old_head;
+        } 
+        while (!free_list.compare_exchange_weak(old_head, new_head, std::memory_order_release, std::memory_order_relaxed));
+    }
+
+    ~safe_memory_pool()
+    {
+        free(pool_start);
+    }
+
+private:
+    void* pool_start;
+    std::atomic<block*> free_list;
 };

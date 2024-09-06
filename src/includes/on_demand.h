dec81163bfa0bff81805641e89e1d858c552cb1b
Yitao Huang <yitao@umich.edu>
2024-09-06 16:14:07 -0700

    add on demand class

---
+++ src/includes/on_demand.h
@@ -0,0 +1,30 @@
+#include <memory>
+
+template <typename T>
+class on_demand
+{
+public:
+    on_demand() = default;
+    ~on_demand() = default;
+    on_demand(const on_demand& rhs) = delete;
+    on_demand(on_demand&& rhs) = delete;
+    on_demand& operator=(const on_demand& rhs) = delete;
+    on_demand& operator=(on_demand&& rhs) = delete;
+
+    T* read() const
+    {
+        return data.get();
+    }
+
+    T* write()
+    {
+        if (!data.get())
+        {
+            data.reset(new T());
+        }
+        return data.get();
+    }
+
+private:
+    std::unique_ptr<T> data;
+};

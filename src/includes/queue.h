624bcb6e9ed9715936e18ee08c6bf296713f543b
Yitao Huang <yitao@umich.edu>
2024-09-09 22:15:29 -0700

    update queue

--- src/includes/queue.h
+++ src/includes/queue.h
@@ -46,121 +46,51 @@ private:
 template<typename T>
 class mpsc_queue {
 public:
-    mpsc_queue(size_t capacity) 
-        : buffer_(capacity), capacity_(capacity), head_(0), tail_(0) {}
+    mpsc_queue(size_t size) 
+        : buffer_(size), size_(size), head_(0), tail_(0) {
+            if (size == 0) {
+                throw std::invalid_argument("Capacity must be greater than zero");
+            }
+        }
 
     bool enqueue(T&& value) {
         size_t current_head = head_.load(std::memory_order_relaxed);
-        size_t next_head = (current_head + 1) % capacity_;
-
-        // Atomically claim a slot for writing
-        while (true) {
+        size_t next_head = (current_head + 1) % size_;
+        // Reserve a slot for this producer (atomic increment of head)
+        do {
             if (next_head == tail_.load(std::memory_order_acquire)) {
-                return false;  // Queue is full
+                return false; // Queue is full
             }
+            next_head = (current_head + 1) % size_;
+        } while (!head_.compare_exchange_weak(current_head, next_head, std::memory_order_acquire, std::memory_order_relaxed));
 
-            // Try to update the head atomically
-            if (head_.compare_exchange_weak(current_head, next_head, std::memory_order_acquire, std::memory_order_relaxed)) {
-                // Successfully claimed the slot, now safe to write
-                buffer_[current_head] = std::move(value);
-                break;
-            }
-            next_head = (current_head + 1) % capacity_;
-        }
+        // Write the value into the reserved slot
+        buffer_[current_head] = std::move(value);
+
+        // Operation successful
         return true;
     }
 
     bool dequeue(T& result) {
-        size_t current_tail = tail_.load(std::memory_order_relaxed); 
+        size_t current_tail = tail_.load(std::memory_order_relaxed);
+
+        // If the queue is empty
         if (current_tail == head_.load(std::memory_order_acquire)) {
-            return false;  // Queue is empty
+            return false; // Queue is empty
         }
 
-        result = std::move(buffer_[current_tail]);
-        tail_.store((current_tail + 1) % capacity_, std::memory_order_release); // Use atomic store for tail
+        // Retrieve the value from the current tail
+        result = buffer_[current_tail];
+
+        // Atomically update the tail (release)
+        tail_.store((current_tail + 1) % size_, std::memory_order_release);
+
         return true;
     }
 
 private:
     std::vector<T> buffer_;
-    const size_t capacity_;
-    std::atomic<size_t> head_;
-    std::atomic<size_t> tail_;
-};
-
-
-template <typename T>
-class mpmc_queue {
-public:
-    explicit mpmc_queue(size_t size)
-        : size_(size), buffer_(size), head_(0), tail_(0) {
-        for (size_t i = 0; i < size_; ++i) {
-            buffer_[i].sequence.store(i, std::memory_order_relaxed);
-        }
-    }
-
-    bool enqueue(T&& data)
-    {
-        size_t head = head_.load(std::memory_order_relaxed);
-        for (;;) {
-            cell& cell_ = buffer_[head % size_];
-            size_t seq = cell_.sequence.load(std::memory_order_acquire);
-            intptr_t diff = (intptr_t)seq - (intptr_t)head;
-            if (diff == 0) 
-            {
-                if (head_.compare_exchange_weak(head, head + 1, std::memory_order_relaxed)) {
-                    cell_.value = std::move(data);
-                    cell_.sequence.store(head + 1, std::memory_order_release);
-                    return true;
-                }
-            } 
-            else if (diff < 0) 
-            {
-                return false; /* full */
-            } 
-            else {
-                head = head_.load(std::memory_order_relaxed);
-            }
-        }
-    }
-
-    bool dequeue(T& data) 
-    {
-        size_t tail = tail_.load(std::memory_order_relaxed);
-        for (;;) 
-        {
-            cell& cell_ = buffer_[tail % size_];
-            size_t seq = cell_.sequence.load(std::memory_order_acquire);
-            intptr_t diff = (intptr_t)seq - (intptr_t)(tail + 1);
-            if (diff == 0) 
-            {
-                if (tail_.compare_exchange_weak(tail, tail + 1, std::memory_order_relaxed)) 
-                {
-                    data = cell_.value;
-                    cell_.sequence.store(tail + size_, std::memory_order_release);
-                    return true;
-                }
-            } 
-            else if (diff < 0) 
-            {
-                return false; /* empty */
-            } 
-            else 
-            {
-                tail = tail_.load(std::memory_order_relaxed);
-            }
-        }
-    }
-
-private:
-    struct cell 
-    {
-        std::atomic<size_t> sequence;
-        T value;
-    };
-
     const size_t size_;
-    std::vector<cell> buffer_;
     std::atomic<size_t> head_;
     std::atomic<size_t> tail_;
 };

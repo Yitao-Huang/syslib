7870bfe00d50e08cb5ae4cd3f8dc7ba89b5bd70d
Yitao Huang <yitao@umich.edu>
2024-09-09 21:23:47 -0700

    move text files

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 int main() {
     try 
     {
-        auto read_res = asyncio::read("../tests/example.txt");
-        auto write_res = asyncio::write("../tests/output.txt", "Hello, IO world!");
+        auto read_res = asyncio::read("../tests/test_files/example.txt");
+        auto write_res = asyncio::write("../tests/test_files/output.txt", "Hello, IO world!");
         LOG(LogLevel::INFO, "Read content: %s", read_res.get().c_str())
         write_res.get();
         LOG(LogLevel::INFO, "Write done")
